a053579578843f2922ab1f1fbe7b10c2fdd072c2
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-04-04 14:22:19 -0400

    updated to parse xml instead of html

--- lib/CVE/cli/web_scraper.rb
+++ lib/CVE/cli/web_scraper.rb
@@ -10,12 +10,13 @@ require_relative 'vulnerability.rb'
 year = "2016" # this will be user input
 page = 'https://cve.mitre.org/data/downloads/allitems-cvrf-year-'+year+'.xml'
 
-parse_page = Nokogiri::HTML(open(page))
+parse_page = Nokogiri::XML(open(page))
 
-#Pry.start(binding)
+Pry.start(binding)
 CVE_LIST = {}
 parse_page.search('vulnerability').map do |element| 
-  CVE_LIST << Vulnerability.new(element.text) 
+  binding.pry
+  CVE_LIST = Vulnerability.new(element) 
 end
 
 #CVE_LIST = parse_page.css('vulnerability').each
