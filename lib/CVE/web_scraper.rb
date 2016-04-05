325851a6cec88211246aac11a64acd16a6c74661
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-04-05 12:50:01 -0400

    created functionality for repo

--- lib/CVE/web_scraper.rb
+++ lib/CVE/web_scraper.rb
@@ -2,7 +2,7 @@ require 'HTTParty'
 require 'open-uri'
 require 'Nokogiri'
 require 'pry'
-#require_relative 'vulnerability.rb'
+require_relative 'vulnerabilityRepo.rb'
 
 # Use when clause to ask user to specify which cve list to get
 # ask user for which year
@@ -12,12 +12,17 @@ page = 'https://cve.mitre.org/data/downloads/allitems-cvrf-year-'+year+'.xml'
 
 parse_page = Nokogiri::HTML(open(page))
 
-@CVE_LIST = {}
-parse_page.search('vulnerability').each do |element| 
-  @CVE_LIST[:vulnerabilities] = element
-  #binding.pry
+@CVE_LIST = []
+parse_page.search('vulnerability').map do |element| 
+  @CVE_LIST << element.text
 end
 
-@CVE_LIST[:vulnerabilities].each do |attributes|
-  Vulnerability.new(attributes)
+@CVE_LIST.each do |attributes|
+  VulnerabilityRepo.new(attributes)
 end
+
+#@CVE_LIST[:vulnerabilities].children.text to return entire vulnerability
+
+#time can't you believe what's become of us
+#Can't judge but sometimes the people who choose to be in our lives
+#just cant. While those who have the opportunity don't
